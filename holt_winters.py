9cd681085f92bbcf34aad7c5ba03a225022dbaf4
Henrik <hgg1998@gmail.com>
2020-11-06 17:26:50 +0100

    Add additive method.

--- holt_winters.py
+++ holt_winters.py
@@ -69,7 +69,6 @@ class HoltWinters:
             else:
                 level = level_initial
                 trend = trend_initial
-                prev_seasonality = 1
                 day_seasonality[i] = \
                     (self.ts[i] / self.moving_average(i, day_period_length) + self.ts[i + day_period_length] / self.moving_average(i + day_period_length, day_period_length)) / 2
                 week_seasonality[i] = \
@@ -113,7 +112,7 @@ class HoltWinters:
 
         initial_guess = np.array([alpha, beta, gamma_day, gamma_week])
         bound_on_variables = ((0, 1), (0, 1), (0, 1), (0, 1))
-        tolerance = 0.9
+        tolerance = 0.1
         self.is_training = True
         variables_optimized = minimize(self.holt_winters_multiplicative_seasonality_extended, initial_guess, bounds=bound_on_variables, tol=tolerance).x
         self.is_training = False
@@ -133,7 +132,7 @@ class HoltWinters:
         else:
             forecast = np.full(len(self.ts) + self.horizon, math.nan)
 
-        level_eq = lambda alpha, observation, level_prev, trend_prev, prev_season : alpha * (observation - prev_season) + (1 - alpha) * (level_prev + trend_prev)
+        level_eq = lambda alpha, observation, level_prev, trend_prev, season_prev : alpha * (observation - season_prev) + (1 - alpha) * (level_prev + trend_prev)
         trend_eq = lambda beta, level, level_prev, trend_prev : beta * (level - level_prev) + (1 - beta) * trend_prev
         season_eq = lambda gamma, observation, level_prev, trend_prev, season_prev : gamma * observation / (level_prev - trend_prev) + (1 - gamma) * season_prev
 
@@ -181,9 +180,78 @@ class HoltWinters:
 
         initial_guess = np.array([alpha, beta, gamma])
         bound_on_variables = ((0, 1), (0, 1), (0, 1))
-        tolerance = 0.8
+        tolerance = 0.1
 
         self.is_training = True
         variables_optimized = minimize(self.holt_winters_multiplicative, initial_guess, bounds=bound_on_variables, tol=tolerance).x
         self.is_training = False
         return self.holt_winters_multiplicative(variables_optimized)
+
+    def holt_winters_additive(self, x):
+        alpha = x[0]    # 0 <= alpha <= 1
+        beta = x[1]     # 0 <= beta <= 1
+        gamma = x[2]    # 0 <= gamma <= 1
+        day_period_length = 24
+        level_prev = self.ts[0]
+        trend_prev = 1
+
+        seasonal_component = np.full(len(self.ts) + self.horizon, math.nan)
+        if self.is_training:
+            forecast = np.full(len(self.ts), math.nan)
+        else:
+            forecast = np.full(len(self.ts) + self.horizon, math.nan)
+
+        level_eq = lambda alpha, observation, level_prev, trend_prev, season_prev : alpha * (observation - season_prev) + (1 - alpha) * (level_prev + trend_prev)
+        trend_eq = lambda beta, level, level_prev, trend_prev : beta * (level - level_prev) + (1 - beta) * trend_prev
+        season_eq = lambda gamma, observation, level_prev, trend_prev, season_prev : gamma * (observation - level_prev - trend_prev) + (1 - gamma) * season_prev
+
+        for i in range(0, len(self.ts)):
+            # Calculate component
+            if i - day_period_length >= 0:
+                level = level_eq(alpha, self.ts[i], level_prev, trend_prev, seasonal_component[i - day_period_length])
+                seasonal_component[i] = season_eq(gamma, self.ts[i], level_prev, trend_prev, seasonal_component[i - day_period_length])
+            else:
+                seasonal_component[i] = 1
+                level = level_eq(alpha, self.ts[i], level_prev, trend_prev, 0)
+            trend = trend_eq(beta, level, level_prev, trend_prev)
+
+            # Make forecasts
+            if (i - day_period_length >= 0):
+                forecast[i] = level + trend + seasonal_component[i - day_period_length]
+            else:
+                forecast[i] = level + trend
+            level_prev = level
+            trend_prev = trend
+
+        if not self.is_training:
+            # Predict
+            forecast = np.full(self.horizon, math.nan)
+            for i in range(len(self.ts), len(self.ts) + self.horizon):
+                trend = trend_eq(beta, level, level_prev, trend_prev)
+                if i == len(self.ts):
+                    level = level_eq(alpha, self.ts[-1], level_prev, trend_prev, seasonal_component[i - day_period_length])
+                    forecast[i - len(self.ts)] = level + trend + seasonal_component[i - day_period_length]
+                    seasonal_component[i] = season_eq(gamma, self.ts[-1], level_prev, trend_prev, seasonal_component[i - day_period_length])
+                else:
+                    level = level_eq(alpha, forecast[i - len(self.ts) - 1], level_prev, trend_prev, seasonal_component[i - day_period_length])
+                    forecast[i - len(self.ts)] = level + trend + seasonal_component[i - day_period_length]
+                    seasonal_component[i] = season_eq(gamma, forecast[i - len(self.ts)], level_prev, trend_prev, seasonal_component[i - day_period_length])
+                level_prev = level
+                trend_prev = trend
+            return forecast
+        else:
+            return self.sse(forecast)
+    
+    def holt_winters_additive_predict(self):
+        alpha = 0.9     # 0 <= alpha <= 1
+        beta = 0.1      # 0 <= beta <= 1
+        gamma = 0.01     # 0 <= gamma <= 1 - alpha
+
+        initial_guess = np.array([alpha, beta, gamma])
+        bound_on_variables = ((0, 1), (0, 1), (0, 1 - alpha))
+        tolerance = 0.1
+
+        self.is_training = True
+        variables_optimized = minimize(self.holt_winters_additive, initial_guess, bounds=bound_on_variables, tol=tolerance).x
+        self.is_training = False
+        return self.holt_winters_additive(variables_optimized)
--- main.py
+++ main.py
@@ -10,50 +10,10 @@ from holt_winters import HoltWinters
 def sse(ts, forecast):
     assert(len(ts) == len(forecast))
     error = []
-    for i in range(100, len(ts)):
+    for i in range(0, len(ts)):
         step_error = pow(ts[i] - forecast[i], 2)
         error.append(step_error)
-    #plt.plot(error)
-    #plt.show()
     return np.sum(error)
-
-def holt_winters_additive(ts, horizon):
-    alpha = 0.7    # 0 <= alpha <= 1
-    beta = 0.5     # 0 <= beta <= 1
-    gamma = 0.001 #1 - alpha    # 0 <= gamma <= 1 - alpha
-    h = 1
-    m = 24
-    k = math.floor((h - 1.0) / float(m))
-    prev_level = 10000
-    prev_trend = 10000
-    forecast = np.full(len(ts) + horizon, math.nan)
-    seasonal_components = np.full(len(ts) + horizon, math.nan)
-
-    level_eq = lambda alpha, ts, prev_level, prev_trend, prev_season : alpha * (ts[0] - prev_season) + (1 - alpha) * (prev_level + prev_trend)
-    trend_eq = lambda beta, level, prev_level, prev_trend : beta * (level - prev_level) + (1 - beta) * prev_trend
-    season_eq = lambda gamma, ts, prev_level, prev_trend, prev_season : gamma * (ts[0] - prev_level - prev_trend) + (1 - gamma) * prev_season
-
-    forecast[1] = ts[0]
-    for i in range(2, len(ts) + 1):
-        # Calculate components
-        if i - m >= 0:
-            level = level_eq(alpha, ts[i - 1], prev_level, prev_trend, seasonal_components[i - m]) # Add seasonality
-            seasonal_components[i - 2] = season_eq(gamma, ts[i - 1], prev_level, prev_trend, seasonal_components[i - m]) 
-        else: 
-            seasonal_components[i - 2] = 0
-            level = level_eq(alpha, ts[i - 1], prev_level, prev_trend, 0)
-        trend = trend_eq(beta, level, prev_level, prev_trend)
-
-        # Make forecasts
-        if (i - m * (k + 1) < 0):
-            forecast[i] = level + h * trend 
-        else:
-            forecast[i] = level + h * trend + seasonal_components[i - m * (k + 1)]
-        prev_level = level
-        prev_trend = trend
-
-    forecast[len(ts) + 1 :] = forecast[len(ts)] # Make prediction
-    return forecast[1:]
 
 def main():
     data = pd.read_csv('data/Demand_for_California_hourly_UTC_time.csv', header=0, infer_datetime_format=True, parse_dates=[0], index_col=[0])
@@ -61,17 +21,23 @@ def main():
     data.index.freq = 'H' # Hourly data.
 
     horizon = 100
-    ts = data.loc['2019-1-1' : '2019-3-31'].to_numpy() # 
+    ts = data.loc['2019-1-1' : '2019-3-31'].to_numpy() 
 
     hw = HoltWinters(ts[:-horizon], horizon)
     forecast_multiplicative = hw.holt_winters_multiplicative_predict()
+    forecast_additive = hw.holt_winters_additive_predict()
     forecast_season_extended = hw.holt_winters_multiplicative_seasonality_extended_predict()
 
-    x = np.arange(0, horizon, 1)
-    plt.plot(x, ts[-horizon:], 'b')
-    plt.plot(x, forecast_multiplicative, 'r')
-    plt.plot(x, forecast_season_extended, 'y')
-    plt.show()
+    print(sse(ts[-horizon:], forecast_additive))
+    print(sse(ts[-horizon:], forecast_multiplicative))
+    print(sse(ts[-horizon:], forecast_season_extended))
+
+    #x = np.arange(0, horizon, 1)
+    #plt.plot(x, ts[-horizon:], 'r')
+    #plt.plot(x, forecast_additive, 'r')
+    #plt.plot(x, forecast_multiplicative, 'r')
+    #plt.plot(x, forecast_season_extended, 'y')
+    #plt.show()
 
 if __name__ == '__main__':
     main()
